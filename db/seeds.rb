20754513168e22a30c2c7680f4252c980422b0fa
ianellison <ianeliison87@yahoo.com>
2019-07-05 16:17:28 -0700

    seed demo user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ActiveRecord::Base.transaction do
+  User.destroy_all
+
+  demo_user = {
+    'username' => 'demoawesomeanimation',
+    'password' => 'passwordpassword',
+    'email' => 'demogod@test.net'
+  }
+
+  User.create!(demo_user)
+end
