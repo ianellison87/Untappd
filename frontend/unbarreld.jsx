8a8dc9575aac478057c46aa9d0e70545bb689acd
ianellison <ianeliison87@yahoo.com>
2019-07-12 12:09:50 -0700

    keep dem dinos with app name

--- frontend/unbarreld.jsx
+++ frontend/unbarreld.jsx
@@ -32,6 +32,6 @@ document.addEventListener("DOMContentLoaded", () => {
 var http = require("http");
 
 setInterval(function () {
-  http.get("http://<your app name>.herokuapp.com");
+  http.get("http://https://unbarreld.herokuapp.com");
 }, 300000); // every 5 minutes (300000)
 
